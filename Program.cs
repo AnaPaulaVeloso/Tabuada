a553d8bba1ac09b87bb1465ab583b4aa93286a6a
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-11-27 23:48:19 -0300

    Concluir a tarefa Tabuada

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador,numero,tabuada;
+            contador = 0;
+
+            Console.Write("Exiba o número:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            while(contador <= 10)
+            {
+                tabuada = numero * contador;
+                Console.WriteLine($"{numero} x {contador} = {tabuada}");
+                contador = contador +1;
+            }
+        }
+    }
+}
